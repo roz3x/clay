0f51d239d705bf638e53de600517739a1f2e368d
Qeubd-Bsjso <kaundalabhishek334@gmail.com>
2021-10-06 21:37:42 +0530

    initial commit

--- clay.py
+++ clay.py
@@ -1,1 +1,14 @@
-print("hello world")
+import sys
+
+file_path = sys.argv[1]
+f = open(file_path , "r")
+
+s = f.read()
+
+while s[len(s)-1] == '\n':
+	s = s[:-1]
+
+
+parts = s.split("\n\n")
+for part in parts:
+	print(part)
